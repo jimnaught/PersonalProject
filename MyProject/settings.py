dde25712fbd5eae4aea81c4b6bc4bd248c4f9341
Jimnaught Mhlanga <jimnaught90@gmail.com>
2024-06-11 10:17:20 +0200

    adding db to settings

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j^q!*w2il6n^=jg2ngo+zd-8g==n93+sxba$haa4x3=k$vnxt4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'YDvGaSmpptKhjkKboJbNadQXTsuEfZdo',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '57727',
     }
 }
 
---
+++ build_files.sh
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+Python 3.10.8 manage.py collectstatic
